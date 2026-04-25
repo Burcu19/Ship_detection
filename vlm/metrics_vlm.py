01b871e8846fce90afea8ad07c3fbe58fddef7f9
Burcu19 <burcu817@hotmail.com>
2026-04-25 18:26:46 +0300

    feat: VLM kodlari eklendi

---
+++ vlm/metrics_vlm.py
@@ -0,0 +1,13 @@
+class Metrics:
+    def __init__(self):
+        pass
+    def accuracy(self):
+        pass
+    def precision(self):
+        pass
+    def recall(self):
+        pass
+    def f1score(self):
+        pass
+    def confusion_matrix(self):
+        pass
---
+++ vlm/vlm.py
@@ -0,0 +1,21 @@
+class VLM:
+    """Buarada döngü içinde (50 adet) vlm modeline (gpt-4o) json formatında çıktı alınacak.
+    JSON formatı için config ayarı yapılacaktır."""
+    """vlm sonucu error vermesş durumunda kayıt işlemleri yapsın kaçıncı fotoya kadar geldiyse."""
+    def __init__(self):
+        self.dongu_sayisi=50
+
+    def get_data(self):
+        pass
+    def prompt(self):
+        pass
+    def  config_parameter(self):
+        pass
+    def call_openai(self):
+        pass
+    def get_result(self):
+        pass
+
+
+
+
